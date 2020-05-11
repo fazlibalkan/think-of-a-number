f543c466977a09902305548f66851bfde369f002
fazlibalkan <53044560+fazlibalkan@users.noreply.github.com>
2020-05-11 20:01:29 +0300

    Add files via upload

---
+++ Assets/Scripts/Guess.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class Guess : MonoBehaviour
+{
+    private static int number = 1, guessedNumber = 0;
+    public Text guessedText;
+    public void Update()
+    {
+        if (SceneManager.GetActiveScene().buildIndex == 8)
+        {
+            guessedText.text = guessedNumber.ToString();
+            
+        }
+    }
+
+    public void PlayGame()
+    {
+        number = 1;
+        guessedNumber = 0;
+        SceneManager.LoadScene(1);
+    }
+    public void NextScene()
+    {
+        Debug.Log(guessedNumber);
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+
+
+    public void ItIs()
+    {
+        guessedNumber += number;
+        number *= 2;
+        NextScene();
+    }
+
+    public void ItIsNot()
+    {
+        number *= 2;
+        NextScene();
+    }
+
+    
+}
